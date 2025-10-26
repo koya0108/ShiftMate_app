a34f842977354b49c203ce8569f41a1ca51152a8
koya <koyamaguchi.4@gmail.com>
2025-10-26 14:58:46 +0900

    lintcheck

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:privacy, :terms, :contact]
+  skip_before_action :authenticate_user!, only: [ :privacy, :terms, :contact ]
 
   def privacy
   end
